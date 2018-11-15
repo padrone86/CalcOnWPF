72d2a1631f07841f3eb677655f4b4d7f8a3ad909
aoki <padrone86@gmail.com>
2018-11-16 01:50:00 +0900

    勉強会用のアプリ作成

---
+++ CalcOnWPF/CalcOnWPF/Calc.cs
@@ -0,0 +1,50 @@
+﻿using Prism.Mvvm;
+
+namespace CalcOnWPF
+{
+    public class Calc : BindableBase
+    {
+        int _num1;
+        int _num2;
+
+        /// <summary>
+        /// 足される数
+        /// </summary>
+        public int Num1
+        {
+            get { return _num1; }
+            set
+            {
+                SetProperty(ref _num1, value);
+                RaisePropertyChanged(nameof(Sum));
+            }
+        }
+
+        /// <summary>
+        /// 足す数
+        /// </summary>
+        public int Num2
+        {
+            get { return _num2; }
+            set
+            {
+                SetProperty(ref _num2, value);
+                RaisePropertyChanged(nameof(Sum));
+            }
+        }
+
+        /// <summary>
+        /// 合計
+        /// </summary>
+        public int Sum { get { return Num1 + Num2; } }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public Calc()
+        {
+            Num1 = 0;
+            Num2 = 0;
+        }
+    }
+}
